edda54250bb0d550008dad337f0fd7a1ead76bab
kimimarooo <94463206+kimimarooo@users.noreply.github.com>
2022-02-05 21:49:48 +0400

    first part of  api project had been created

--- main.py
+++ main.py
@@ -1,42 +1,71 @@
 import requests
 import sys
 from PyQt5 import uic
-
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+def request_map_image(x, y, zoom, size):
+    req = 'https://static-maps.yandex.ru/1.x/'
+    req += '?ll=' + str(x) + ',' + str(y)
+    req += '&spn=' + str(zoom[0]) + ',' + str(zoom[1])
+    req += '&size=' + str(size[0]) + ',' + str(size[1])
+    req += '&l=sat'
+    response = requests.get(req)
+    return req, response, response.content
+
+
+def request_map_objects(name):
+    req = "http://geocode-maps.yandex.ru/1.x/"
+    req += '?apikey=' + API_KEY
+    req += "&geocode=" + name
+    req += "&format=json"
+    response = requests.get(req)
+    if not response:
+        return req, False, None
+    json = response.json()
+    return req, True, json["response"]["GeoObjectCollection"]["featureMember"]
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main_window.ui', self)  
+        uic.loadUi('main_window.ui', self)
         self.setWindowTitle('API-Project')
         self.search_btn.clicked.connect(self.run)
+        self.quit_btn.clicked.connect(self.quit)
+
+    def quit(self):
+        sys.exit(0)
 
     def run(self):
-        if self.geo_width_input.text() and self.geo_height_input.text():
-            latitude = self.geo_width_input.text()
-            longitude = self.geo_height_input.text()
+        if self.latitude_input.text() and self.longitude_input.text():
+            latitude = self.latitude_input.text()
+            longitude = self.longitude_input.text()
+            try:
+                zoom = float(self.zoom_input.text())
+            except:
+                zoom = 1.0
         else:
-            self.close()
-        map_request = f"https://static-maps.yandex.ru/1.x/?ll={latitude},{longitude}&spn=0.016457,0.00619&l=map"
-        response = requests.get(map_request)
+            return
+        req, response, content = request_map_image(latitude, longitude, (zoom, zoom), (573, 430))
 
         if not response:
-            print("Ошибка выполнения запроса:")
-            print(map_request)
+            print("Ошибка выполнения запроса: " + req)
             print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-        self.pixmap = QPixmap('map.png')
-        self.image = QLabel(self)
-        self.image.resize(573, 430)
-        self.image.move(10, 10)
-        self.image.setPixmap(self.pixmap)
-        self.image.show()
+        try:
+            pixmap = QPixmap()
+            pixmap.loadFromData(content)
+            self.pixmap = pixmap
+            self.image = QLabel(self)
+            self.image.resize(573, 430)
+            self.image.move(10, 10)
+            self.image.setPixmap(self.pixmap)
+            self.image.show()
+        except Exception as e:
+            print('Exception: ' + str(e))
 
 
 if __name__ == '__main__':
